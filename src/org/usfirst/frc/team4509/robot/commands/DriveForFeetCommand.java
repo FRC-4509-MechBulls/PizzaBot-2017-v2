6c49b7f0d2002f4da4caaea0544e736997d26cc7
Kyle Brott <ltcnugget@gmail.com>
2017-11-08 14:59:46 -0500

    implemented DriveForFeetCommand

---
+++ src/org/usfirst/frc/team4509/robot/commands/DriveForFeetCommand.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team4509.robot.commands;
+
+import org.usfirst.frc.team4509.robot.Robot;
+import org.usfirst.frc.team4509.robot.subsystems.DrivingSubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.Timer;
+
+public class DriveForFeetCommand extends Command {
+	
+	private boolean isFinished;
+	private double secondsToDriveFor;
+	
+	public DriveForFeetCommand(double feet) {
+		requires(Robot.drivingSubsystem);
+		this.isFinished = false;
+		this.secondsToDriveFor = feet * DrivingSubsystem.FEET_PER_SECOND;
+	}
+
+	@Override
+	protected void initialize() {
+		Robot.drivingSubsystem.setSpeed(0);
+	}
+
+	@Override
+	protected void execute() {
+		Timer timer = new Timer();
+		double startTime = timer.get();
+		Robot.drivingSubsystem.setSpeed(DrivingSubsystem.BASE_SPEED);
+		while(timer.get() - startTime < this.secondsToDriveFor) {}
+		this.isFinished = true;
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return this.isFinished;
+	}
+
+	@Override
+	protected void end() {
+		Robot.drivingSubsystem.setSpeed(0);
+	}
+
+	@Override
+	protected void interrupted() {}
+
+}
